3f325641e03a00aa0133df74b91775b152564cc2
biso <bopoku89@gmail.com>
2024-06-10 08:42:56 -0400

    Add lab with registration elements

---
+++ index.js
@@ -0,0 +1,6 @@
+const regForm = document.getElementById("registration")
+const username = registration.elements["username"]
+const email = registration.elements["email"]
+const password = registration.elements["password"]
+const passwordCheck = registration.elements["passwordCheck"]
+
